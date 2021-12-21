2122f3fb5387838c795a3e4f30c92132eb17c429
samchencode <samchencode@gmail.com>
2021-12-21 08:14:44 -0500

    feat: remove boilerplate

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,6 @@
 import { Injector } from 'didi';
-import type Stack from '@/domain/Stack';
 import dependencies from '@/dependencies';
 
 const container = new Injector([dependencies]);
-const stack = container.get<Stack<any>>('stack');
-
-console.log(stack);
 
 container.get('test');
--- src/dependencies.ts
+++ src/dependencies.ts
@@ -1,9 +1,7 @@
 import { Module } from 'didi';
-import Stack from '@/domain/Stack';
 
 const dependencies = new Module();
 
-dependencies.type('stack', Stack);
 dependencies.factory('test', function (key: string) {
   console.log(key);
 });
