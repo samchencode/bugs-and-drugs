9f9002eae77cf9f52355b25b299dfb8d7f3da39b
AndrewGoldmann <agoldmann@outlook.com>
2021-12-27 10:13:01 -0500

    feat: buildTableFromAntibiogram service

--- src/domain/buildTableFromAntibiogram.ts
+++ src/domain/buildTableFromAntibiogram.ts
@@ -1,5 +1,57 @@
-function buildTableFromAntibiogram() {
-  //
+import Antibiogram from '@/domain/Antibiogram';
+import type { Cell } from './Table/Cell';
+import makeTable from './Table/makeTable';
+import SensitivityData from './SensitivityData';
+
+interface AntibiogramTableCell extends Cell<string> {
+  value: string;
+}
+
+class EmptyAntibiogramTableCell implements AntibiogramTableCell {
+  constructor() {
+    this.value = 'NA';
+  }
+  value: string;
+}
+
+class FilledAntibiogramTableCell implements AntibiogramTableCell {
+  constructor(data: SensitivityData) {
+    this.value = data.value.toString();
+  }
+  value: string;
+}
+
+function buildTableFromAntibiogram(antibiogram: Antibiogram) {
+  if(antibiogram.isEmpty()) return makeTable([]);
+  const { antibiotics, organisms } = antibiogram;
+  const labels = {
+    rows: organisms.map((o) => o.name),
+    columns: antibiotics.map((a) => a.name),
+  };
+  const nRows = labels.rows.length;
+  const nColumns = labels.columns.length;
+  let cells: AntibiogramTableCell[][] = makeEmptyMatrix(nRows, nColumns);
+
+  for (const d of antibiogram.getData()) {
+    const row = labels.rows.indexOf(d.organism.name);
+    const column = labels.columns.indexOf(d.antibiotic.name);
+    cells[row][column] = new FilledAntibiogramTableCell(d);
+  }
+
+  return makeTable(cells, labels);
+}
+
+function makeEmptyMatrix(
+  nRows: number,
+  nColumns: number
+): EmptyAntibiogramTableCell[][] {
+  return new Array(nRows)
+    .fill(undefined)
+    .map(() =>
+      new Array(nColumns)
+        .fill(undefined)
+        .map(() => new EmptyAntibiogramTableCell())
+    );
 }
 
 export default buildTableFromAntibiogram;
---
+++ tests/domain/buildTableFromAntibiogram.spec.ts
@@ -0,0 +1,58 @@
+import buildTableFromAntibiogram from '@/domain/buildTableFromAntibiogram';
+import Antibiogram from '@/domain/Antibiogram';
+import SensitivityData from '@/domain/SensitivityData';
+import Table from '@/domain/Table';
+import Organism from '@/domain/Organism';
+import Antibiotic from '@/domain/Antibiotic';
+import SensitivityValue from '@/domain/SensivityValue/SensitivityValue';
+
+describe('make table using antibiogram', () => {
+  it('creates table using empty antibiogram', () => {
+    let abg = new Antibiogram([]);
+    const table = buildTableFromAntibiogram(abg);
+    expect(table).toBeInstanceOf(Table);
+    expect(table.getData()).toEqual([]);
+  });
+
+  describe('with data', () => {
+    let abg: Antibiogram;
+
+    beforeEach(() => {
+      const data = [
+        new SensitivityData({
+          organism: new Organism(1, 'Klebsiella'),
+          antibiotic: new Antibiotic(1, 'Azithromycin'),
+          value: new SensitivityValue('100'),
+        }),
+        new SensitivityData({
+          organism: new Organism(2, 'Pseudomonas'),
+          antibiotic: new Antibiotic(1, 'Azithromycin'),
+          value: new SensitivityValue('R'),
+        }),
+        new SensitivityData({
+          organism: new Organism(3, 'Staph aureus'),
+          antibiotic: new Antibiotic(1, 'Azithromycin'),
+          value: new SensitivityValue('90'),
+        }),
+      ];
+      abg = new Antibiogram(data);
+    });
+
+    it('creates table using antibiogram', () => {
+      const table = buildTableFromAntibiogram(abg);
+      expect(table.getRowLabels()).toEqual(
+        expect.arrayContaining(['Klebsiella', 'Pseudomonas', 'Staph aureus'])
+      );
+      expect(table.getColumnLabels()).toEqual(
+        expect.arrayContaining(['Azithromycin'])
+      );
+      expect(table.getData()).toEqual(
+        expect.arrayContaining([
+          [{ value: '100%' }],
+          [{ value: 'R' }],
+          [{ value: '90%' }],
+        ])
+      );
+    });
+  });
+});
