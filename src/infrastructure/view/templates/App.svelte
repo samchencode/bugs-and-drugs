7724808619aed5b078f6d88a0b5f3654483314dc
AndrewGoldmann <agoldmann@outlook.com>
2021-12-29 11:19:55 -0500

    Display table in app.svelte

--- src/infrastructure/view/templates/App.svelte
+++ src/infrastructure/view/templates/App.svelte
@@ -1,44 +1,23 @@
 <script lang="ts">
-  import { getContext } from 'svelte';
-  import MyCmpt from './common/MyCmpt.svelte';
-
-  const tableController: any = getContext('tableController');
-  tableController.showTable().then((table: any) => {
-    console.log(table);
-  });
-
-  let name: string = 'bugs and drugs';
-  let msg: string;
-  $: label = 'you typed: ' + msg;
+  import Table from './table/Table.svelte';
+  import './index.css';
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <form action="">
-    <input type="text" name="msg" bind:value={msg} />
-    <label for="msg">{label}</label>
-  </form>
-  <MyCmpt {msg} />
+  <div class="container">
+    <!-- <div class="title"><h1>Hello World!</h1></div>
+    <div class="filter-selector">
+      this is where the filter selector bar will be
+    </div>
+    <div class="table-block">
+    </div> -->
+    <Table />
+    <!-- <div class="favorites">favorits</div>
+    <div class="disclaimer">disclaimer</div> -->
+    <!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
+  </div>
 </main>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
+<!-- BEM : block__element--modifier -->
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+<!-- .Form input.form__input button.form__button button.form__button--disabled -->
