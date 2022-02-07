dbab6c6db2ae345421d7a2744eba62557609739b
samchencode <samchencode@gmail.com>
2022-02-07 17:09:05 -0500

    feat: remove fullscreen div

--- src/infrastructure/view/templates/App.svelte
+++ src/infrastructure/view/templates/App.svelte
@@ -5,18 +5,6 @@
   import IndexScreen from './index-screen/IndexScreen.svelte';
 </script>
 
-<main>
-  <Navigation />
-  <!-- <TableViewScreen /> -->
-  <IndexScreen />
-</main>
-
-<style>
-  main {
-    position: relative;
-    height: 100%;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-  }
-</style>
+<Navigation />
+<!-- <TableViewScreen /> -->
+<IndexScreen />
