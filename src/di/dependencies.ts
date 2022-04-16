8b9a1885e022baf8de2feda98528b5b1a48c90d2
samchencode <samchencode@gmail.com>
2022-04-16 19:06:55 -0400

    refactor: remove unused classes

--- src/di/dependencies.ts
+++ src/di/dependencies.ts
@@ -8,8 +8,6 @@ import IndexAntibiogramAction from '@/application/IndexAntibiogramAction';
 import WebAntibiogramGroupsPresenter from '@/infrastructure/view/presenters/WebAntibiogramGroupPresenter';
 import WebFileSystem from '@/infrastructure/filesystem/web/WebFileSystem';
 import IndexAntibiogramTitleAction from '@/application/IndexAntibiogramTitleAction';
-import AntibiogramTitleController from '@/infrastructure/view/controllers/AntibiogramTitleController';
-import WebAntibiogramTitlePresenter from '@/infrastructure/view/presenters/WebAntibiogramTitlePresenter';
 
 const dependencies = new Module();
 
@@ -26,13 +24,11 @@ dependencies.type('filesystem', WebFileSystem);
 // * - Controllers
 dependencies.type('tableController', TableController);
 dependencies.type('antibiogramGroupController', AntibiogramGroupController);
-dependencies.type('antibiogramTitleController', AntibiogramTitleController);
 // * - Presenter
 dependencies.type(
   'webAntibiogramGroupsPresenter',
   WebAntibiogramGroupsPresenter
 );
-dependencies.type('webAntibiogramTitlePresenter', WebAntibiogramTitlePresenter);
 
 // * INJECTED EXTERNAL DEPENDENCIES
 dependencies.value('fetch', fetch.bind(window));
--- src/di/svelte.ts
+++ src/di/svelte.ts
@@ -1,19 +1,16 @@
 import App from '@/infrastructure/view/templates/App.svelte';
 import type TableController from '@/infrastructure/view/controllers/TableController';
 import type AntibiogramGroupController from '@/infrastructure/view/controllers/AntibiogramGroupController';
-import type AntibiogramTitleController from '@/infrastructure/view/controllers/AntibiogramTitleController';
 
 const svelte = (
   tableController: TableController,
-  antibiogramGroupController: AntibiogramGroupController,
-  antibiogramTitleController: AntibiogramTitleController
+  antibiogramGroupController: AntibiogramGroupController
 ) => {
   return new App({
     target: document.body,
     context: new Map<string, unknown>([
       ['tableController', tableController],
       ['antibiogramGroupController', antibiogramGroupController],
-      ['antibiogramTitleController', antibiogramTitleController],
     ]),
   });
 };
