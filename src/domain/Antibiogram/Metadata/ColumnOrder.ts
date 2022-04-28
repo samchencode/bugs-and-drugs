7170edf2047a8389e9538bc1f4506d7d82aa8b7e
samchencode <samchencode@gmail.com>
2022-04-27 22:29:27 -0400

    fix: typed but not set slug

--- src/domain/Antibiogram/Metadata/ColumnOrder.ts
+++ src/domain/Antibiogram/Metadata/ColumnOrder.ts
@@ -1,7 +1,7 @@
 import Order from '@/domain/Antibiogram/Metadata/Order';
 
 class ColumnOrder extends Order {
-  static readonly slug: 'column-order';
+  static readonly slug = 'column-order';
 
   getSlug() {
     return ColumnOrder.slug;
--- src/domain/Antibiogram/Metadata/RowOrder.ts
+++ src/domain/Antibiogram/Metadata/RowOrder.ts
@@ -1,7 +1,7 @@
 import Order from '@/domain/Antibiogram/Metadata/Order';
 
 class RowOrder extends Order {
-  static readonly slug: 'row-order';
+  static readonly slug = 'row-order';
 
   getSlug() {
     return RowOrder.slug;
