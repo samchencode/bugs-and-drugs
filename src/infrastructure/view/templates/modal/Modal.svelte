7cc816aa4c38caa6f91a42ec09b67eaf26e45406
samchencode <samchencode@gmail.com>
2022-05-06 19:28:03 -0400

    feat: modal styling

--- src/infrastructure/view/templates/modal/Modal.svelte
+++ src/infrastructure/view/templates/modal/Modal.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { modal } from '@/infrastructure/view/templates/modal/ModalStore';
   import { fade } from 'svelte/transition';
+  modal.showAbout();
 </script>
 
 {#if !$modal.hidden}
@@ -13,17 +14,16 @@
     <div class="modal-header">
       <h1 class="modal-header-text">{$modal.title}</h1>
     </div>
-
     <p class="modal-text">
       {$modal.text}
     </p>
-    <h2
+    <button
       name="close-outline"
-      class="close-modal clickable"
+      class="close-modal"
       on:click={() => modal.hideModal()}
     >
-      CLOSE
-    </h2>
+      Close
+    </button>
   </div>
 {/if}
 
@@ -35,45 +35,53 @@
     width: 100vw;
     height: 100vh;
     background-color: black;
-    opacity: 50%;
-    z-index: 500;
+    opacity: 0.5;
+    z-index: 1000;
   }
+
   .modal-container {
-    display: grid;
-    grid-template-columns: var(--modal-width);
-    grid-template-rows: 40px auto 52px;
     position: fixed;
-    top: 10%;
+    top: 50%;
     left: 50%;
+    transform: translate(-50%, -50%);
     z-index: 1000;
     background-color: white;
-    border: 1px solid black;
-    transform: translateX(-50%);
     box-shadow: var(--bs);
+    width: var(--modal-width);
+    display: grid;
+    grid-template: 24px auto auto 52px / 24px auto 16px 8px;
   }
+
   .modal-header {
-    grid-row: 1;
-    text-align: left;
-    padding-left: 24px;
+    grid-row: 2 / span 1;
+    grid-column: 2 / span 1;
+    margin-bottom: var(--space-md);
   }
+
   .modal-header-text {
-    font: sans-serif;
     font-weight: bold;
     font-size: var(--font-lg);
   }
-  .close-modal {
-    grid-row: 3;
-    text-align: right;
-    padding: 10px;
-    font-size: var(--font-lg);
-  }
+
   .modal-text {
-    padding-left: 24px;
-    padding-right: 24px;
-    grid-row: 2;
-    font-size: var(--font-lg);
-    font: sans-serif;
+    grid-row: 3 / span 1;
+    grid-column: 2 / span 1;
+    font-size: var(--font-md);
     max-height: 60vh;
-    overflow: scroll;
+    overflow: auto;
+    margin-bottom: var(--space-md);
+  }
+
+  .close-modal {
+    border: none;
+    background: none;
+    padding: var(--space-xs) var(--space-sm);
+    margin: var(--space-xs) 0;
+    font-size: var(--font-md);
+    font-weight: bold;
+    width: fit-content;
+    justify-self: end;
+    grid-column: -4 / span 2;
+    grid-row: -2 / span 1;
   }
 </style>
