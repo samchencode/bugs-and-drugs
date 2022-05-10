7e9133f3eeaa718eb0c95cc785a2e973e988534e
samchencode <samchencode@gmail.com>
2022-05-09 22:41:45 -0400

    feat: new about us text

--- src/infrastructure/view/templates/dialogues/about.ts
+++ src/infrastructure/view/templates/dialogues/about.ts
@@ -1,7 +1,7 @@
 import type { Dialogue } from '@/infrastructure/view/templates/dialogues/Dialogue';
 
 const about: Dialogue = {
-  body: 'This application is currently under development by medical students Sam Chen and Andrew Goldmann under the direction of Dr. Joshua Steinberg',
+  body: 'Written to put antibiograms in the hands of local and regional clinicians to serve patient care in Upstate New York.  Developed by Sam Chen and Andrew Goldman, class of 2023, SUNY Upstate Medical University College of Medicine, with direction from Dr. Joshua Steinberg, SUNY Upstate Clinical Campus, Binghamton, NY.',
   header: 'About Us',
   dismiss: 'Dismiss',
 };
