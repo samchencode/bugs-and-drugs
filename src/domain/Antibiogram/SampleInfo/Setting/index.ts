d207fde559f07218ab4dc5cdf0605f00a6f3a2e2
AndrewGoldmann <agoldmann@outlook.com>
2023-02-15 11:52:35 -0600

    feat: added long term care to sample data

--- src/domain/Antibiogram/SampleInfo/Setting/index.ts
+++ src/domain/Antibiogram/SampleInfo/Setting/index.ts
@@ -1,6 +1,7 @@
 import Setting from '@/domain/Antibiogram/SampleInfo/Setting/Setting';
 import InPatient from '@/domain/Antibiogram/SampleInfo/Setting/InPatient';
 import OutPatient from '@/domain/Antibiogram/SampleInfo/Setting/OutPatient';
+import LongTermCare from '@/domain/Antibiogram/SampleInfo/Setting/longTermCare';
 
 const Settings = {
   get INPATIENT() {
@@ -8,6 +9,9 @@ const Settings = {
   },
   get OUTPATIENT() {
     return new Setting(new OutPatient());
+  },
+  get LTC() {
+    return new Setting(new LongTermCare());
   },
 };
 
